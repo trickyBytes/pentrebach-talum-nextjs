045fde7ff1d1c631ec5f595dda9e69bb0ea70563
trickyBytes <richard.tiffin@ovo.com>
2024-08-13 16:49:26 +0100

    Applying initial layout experiment

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (<div>
+    <div className="row">
+      Header
+    </div>
+    <div className="row">
+      Hero Section
+    </div>
+    <div id="contentSection" className="row">
+      <div className="row">
+        <div className={"title-all-caps"}>Rural Relaxation on the Snowdonian Coast</div>
+        <h2>Eyri, where the sea meets mountains</h2>
+      </div>
+      <div className="row">
+        <div className="col-1-of-3">Stunning Sea Views</div>
+        <div className="col-1-of-3">Our Properties</div>
+        <div className="col-1-of-3">Activities</div>
+      </div>
+    </div>
+  </div>
+  );
+}
