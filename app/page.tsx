a8e9258e906d3511747cc35ceab8373c9a516e90
trickyBytes <richard.tiffin@ovo.com>
2024-09-07 12:53:07 +0100

    105. Set-up project

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,3 @@
 export default function Home() {
-    return (
-        <>
-            <header>
-                <div className="header__logo-box">
-                    Img goes here....
-                </div>
-            </header>
-
-            <section className="row">
-                Hero Section
-            </section>
-
-            <section id={"contentSection"} className={"row"}>
-                <div className={"row"}>
-                    <div className={"title-all-caps"}>Rural Relaxation on the Snowdonian Coast</div>
-                    <h2>Eyri, where the sea meets mountains</h2>
-                </div>
-                <div className="row">
-                    <div className="col-1-of-3">Stunning Sea Views</div>
-                    <div className="col-1-of-3">Our Properties</div>
-                    <div className="col-1-of-3">Activities</div>
-
-
-                </div>
-            </section>
-
-
-            <div>wibble</div>
-        </>
-    );
+  return 'TEST TEXT';
 }
