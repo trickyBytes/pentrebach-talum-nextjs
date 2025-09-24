a53a2d09b52f1076fecc8c975720d9604ae13394
trickyBytes <richard.tiffin@ovo.com>
2025-09-24 20:27:46 +0100

    WIP: Adding intro sections, grid layour for items

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,40 @@ export default function Home() {
             </section>
 
             <section className="intro">
-                <div>
+                {/* <div>
                     <div className="title-all-caps">Rural Relaxation on the Snowdonian Coast</div>
                     <h2>Eyri, where the sea meets mountains</h2>
+                </div> */}
+                <div className="item">
+                    <svg className="item_icon">
+                        <use xlinkHref="img/sprite.svg#icon-global"></use>
+                    </svg>
+                    <h4 className="heading">
+                        Stunning Sea Views
+                    </h4>
+                    <p className="item__text">
+                        All our guests can enjoy truly spectacular sunsets from our patios as well as from window seats and conservatories. If you’re lucky you might even spot a pod of dolphins swimming past on their way to the beautiful Mawddach Estuary.
+                    </p>
                 </div>
-                <div className="row">
-                    <div className="col-1-of-3">Stunning Sea Views</div>
-                    <div className="col-1-of-3">Our Properties</div>
-                    <div className="col-1-of-3">Activities</div>
+                <div className="item">
+                    <svg className="item_icon">
+                        <use xlinkHref="img/sprite.svg#icon-global"></use>
+                    </svg>
+                    <h4 className="heading">
+                        Our Properties
+                    </h4>
+                    <p className="item__text">
+                        All our guests can enjoy truly spectacular sunsets from our patios as well as from window seats and conservatories. If you’re lucky you might even spot a pod of dolphins swimming past on their way to the beautiful Mawddach Estuary.
+                    </p>
+                </div>
+                <div className="item">
+                    <svg className="item_icon">
+                        <use xlinkHref="img/sprite.svg#icon-global"></use>
+                    </svg>
+                    <h4 className="heading">Activities</h4>
+                    <p className="item__text">
+                        Take a 10 minute stroll to the beach or an energetic game of rounders in our large games field. Many more activities such as pony trekking, stand-up-paddleboarding and mountain biking are just a short drive away.
+                    </p>
                 </div>
             </section>
         </>
